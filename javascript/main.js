fc9347286c4dee509bde4e18371c28d32fd98fb3
Ashik Ahmed <75107616+Ashik7@users.noreply.github.com>
2020-11-27 17:37:25 +0600

    add

---
+++ javascript/main.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+    $('.wLi1').mouseenter(function(){
+        $('.wp1').show('slow');
+    });
+    $('.wLi1').mouseleave(function(){
+        $('.wp1').hide('slow');
+    });
+});
+
+$(document).ready(function(){
+    $('.wLi2').mouseenter(function(){
+        $('.wp2').show('slow');
+    });
+    $('.wLi2').mouseleave(function(){
+        $('.wp2').hide('slow');
+    });
+});
+
+$(document).ready(function(){
+    $('.wLi3').mouseenter(function(){
+        $('.wp3').show('slow');
+    });
+    $('.wLi3').mouseleave(function(){
+        $('.wp3').hide('slow');
+    });
+});
+
+$(document).ready(function(){
+    $('.wLi4').mouseenter(function(){
+        $('.wp4').show('slow');
+    });
+    $('.wLi4').mouseleave(function(){
+        $('.wp4').hide('slow');
+    });
+});
+
+$(document).ready(function(){
+    $('.wLi5').mouseenter(function(){
+        $('.wp5').show('slow');
+    });
+    $('.wLi5').mouseleave(function(){
+        $('.wp5').hide('slow');
+    });
+});
+
+$(document).ready(function(){
+    $('.wLi6').mouseenter(function(){
+        $('.wp6').show(200);
+    });
+    $('.wLi6').mouseleave(function(){
+        $('.wp6').hide(200);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+/*$(document).ready(function(){
+    $('.hide').click(function(){
+        $('.demo').hide(500);
+    });
+    $('.show').click(function(){
+        $('.demo').show(500);
+    });
+}); */
